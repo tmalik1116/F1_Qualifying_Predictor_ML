44a5faf714224261bb9e5301be2f4c3319ddb2ae
Talha Malik <tmalik2004@gmail.com>
2024-08-22 13:03:23 -0400

    Added data for other races betwene 2018-2024

--- read_avg_grid.py
+++ read_avg_grid.py
@@ -18,28 +18,29 @@ def is_rain(session) -> bool:
     else:
         return False
 
-session = fastf1.get_session(2023, "Australia", "Q")
+session = fastf1.get_session(2019, "Hockenheim", "Q")
 session.load(weather=True)
 
 drivers = pd.unique(session.laps['Driver'])
 
-for i in range(20):
-    print(str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))
-    print(str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']).__contains__('nan'))
-
-# avg_time = 0.0
-# num = 20
 # for i in range(20):
-#     try:
-#         avg_time += convert_time(re.findall(r'\d{2}\:\d{2}\.\d{3}', str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))[0])
-#     except:
-#         try:
-#             avg_time += convert_time(re.findall(r'(01:\d{2})', str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))[0] + ".000")
-#         except:
-#             num -= 1
-#             continue
+#     print(str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))
+#     print(str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']).__contains__('nan'))
 
-# avg_time /= num
+avg_time = 0.0
+num = 20
+for i in range(20):
+    try:
+        avg_time += convert_time(re.findall(r'\d{2}\:\d{2}\.\d{3}', str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))[0])
+    except:
+        try:
+            avg_time += convert_time(re.findall(r'(01:\d{2})', str(session.laps.pick_drivers(drivers[i]).pick_fastest()['LapTime']))[0] + ".000")
+        except:
+            num -= 1
+            continue
+
+avg_time /= num
+print(avg_time)
 
 
 #################################
