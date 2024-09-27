fa34868de016bf0126fe3fae85833acfc5f2184e
Talha Malik <tmalik2004@gmail.com>
2024-09-27 16:24:28 -0400

    added more UI elements and animations

---
+++ static/script.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll('.main-button');
+
+// buttons.forEach(button => {
+//     button.addEventListener('mousedown', () => {
+//         button.style.backgroundColor = 'crimson';
+//     });
+
+//     button.addEventListener('mouseup', () => {
+//         button.style.backgroundColor = '';
+//     })
+// })
