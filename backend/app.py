b31138428a08bfa30d8b71046a8fe1ef31fc453b
tmalik1116 <146396156+tmalik1116@users.noreply.github.com>
2024-10-02 20:33:06 +0000

    trying to figure out hosting

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__,
            static_folder='../frontend/build/static',
-           template_folder='../frontend/public')
+           template_folder='../frontend/build')
 
 @app.route('/')
 def index():
