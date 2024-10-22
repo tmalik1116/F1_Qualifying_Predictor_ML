99c69aa80a81a520efcd019a038b3713e44db570
Talha Malik <146396156+tmalik1116@users.noreply.github.com>
2024-10-22 18:02:12 -0400

    Delete legacy_implementation directory

--- legacy_implementation/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "../build/static/styles.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root")); // Target the 'root' div in index.html
-
-root.render(<App />);
