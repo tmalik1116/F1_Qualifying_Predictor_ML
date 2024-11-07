16dce3159a8e31d4e780d394d5b4896e30eaf739
Talha Malik <146396156+tmalik1116@users.noreply.github.com>
2024-11-07 19:34:09 +0000

    messing with switches, normal style works because of how I am implementing it

--- f1-app/src/DriverMenu.js
+++ f1-app/src/DriverMenu.js
@@ -1,47 +1,75 @@
-import React, {useState} from "react";
-import CloseButton from "./CloseButton"
-import CustomizedSwitches from "./CustomizedSwitches"
+import React, { useState } from "react";
+import CloseButton from "./CloseButton";
+import CustomizedSwitch from "./CustomizedSwitches";
+import { Switch } from "@mui/material";
 
-export default function DriverMenu(props){
-    const [isRain, setIsRain] = useState(true); // State to manage the Switch
+export default function DriverMenu(props) {
+  const [isChecked, setIsChecked] = useState(true); // State to manage the Switch
 
-    const handleChange = (event) => {
-        setIsRain(event.target.checked);
-    };
+  const handleChange = (event) => {
+    setIsChecked(event.target.checked);
+  };
 
-    return (
-        <div>
-            <div className="col" id="submenu-column">
-                <div className="row" id="driver-submenu-top">
-                    <label className="input-label-top" for="Driver">Driver</label>
-                    <CloseButton closeSubmenu={props.closeSubmenu}/>
-                </div>
-                <input type="text" id="Driver" placeholder="Ex. LEC, SAI" className="input-field"></input>
-
-                <div className="vertical-spacer-medium"></div> {/* Make 'spacer' CSS class for a div, can reuse wherever I want */}
-
-                <div className="row" id="driver-submenu-top">
-                    <label className="input-label" for="Race">Race</label>
-                </div>
-                <input type="text" id="Race" placeholder="Ex. Monaco, Bahrain" className="input-field"></input>
-
-                <div className="vertical-spacer-medium"></div>
-
-                <div className="row" id="driver-submenu-top">
-                    <label className="input-label" for="Season">Season</label>
-                </div>
-                <input type="text" id="Season" placeholder="Ex. 2024" className="input-field"></input>
-
-                <div className="vertical-spacer-medium"></div>
-
-                <div className="row" id="driver-submenu-top">
-                    <label className="input-label" for="Rain">Rain? (On/Off) {isRain ? 'True': 'False'}</label>
-                </div>
-                <div className="switch">
-                    <CustomizedSwitches color="" id="Rain" checked={isRain} onChange={handleChange}/>
-                </div>
-                
-            </div>
+  return (
+    <div>
+      <div className="col" id="submenu-column">
+        <div className="row" id="driver-submenu-top">
+          <label className="input-label-top" for="Driver">
+            Driver
+          </label>
+          <CloseButton closeSubmenu={props.closeSubmenu} />
         </div>
-    )
+        <input
+          type="text"
+          id="Driver"
+          placeholder="Ex. LEC, SAI"
+          className="input-field"
+        ></input>
+        <div className="vertical-spacer-medium"></div>{" "}
+        {/* Make 'spacer' CSS class for a div, can reuse wherever I want */}
+        <div className="row" id="driver-submenu-top">
+          <label className="input-label" for="Race">
+            Race
+          </label>
+        </div>
+        <input
+          type="text"
+          id="Race"
+          placeholder="Ex. Monaco, Bahrain"
+          className="input-field"
+        ></input>
+        <div className="vertical-spacer-medium"></div>
+        <div className="row" id="driver-submenu-top">
+          <label className="input-label" for="Season">
+            Season
+          </label>
+        </div>
+        <input
+          type="text"
+          id="Season"
+          placeholder="Ex. 2024"
+          className="input-field"
+        ></input>
+        <div className="vertical-spacer-medium"></div>
+        <div className="row" id="driver-submenu-top">
+          <label className="input-label" for="Rain">
+            Rain? (On/Off) {isChecked ? "True" : "False"}
+          </label>
+        </div>
+        <div className="switch">
+          <CustomizedSwitch
+            id="Rain"
+            checked={isChecked}
+            onChange={handleChange}
+            inputProps={{ "ari-label": "controlled" }}
+          />
+          <Switch
+            checked={isChecked}
+            onChange={handleChange}
+            inputProps={{ "ari-label": "controlled" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
