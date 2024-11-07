16720a51751461e182670d7fe84616369077e718
Talha Malik <146396156+tmalik1116@users.noreply.github.com>
2024-11-07 17:00:49 +0000

    will deal with animation later, get everything working first

--- f1-app/src/MainButton.js
+++ f1-app/src/MainButton.js
@@ -2,16 +2,15 @@ import React from "react";
 import DriverMenu from "./DriverMenu";
 
 export default function MainButton(props) {
+  function closeSubmenu() {
+    props.toggleSubmenu(props.type);
+  }
 
-    function closeSubmenu(){
-        props.toggleSubmenu(props.type);
+  function handleButtonClick() {
+    if (!props.isActive) {
+      props.toggleSubmenu(props.type);
     }
-
-    function handleButtonClick(){
-        if (!props.isActive){
-            props.toggleSubmenu(props.type);
-        }
-    }
+  }
 
   return (
     <div className="col-6">
@@ -22,12 +21,14 @@ export default function MainButton(props) {
       >
         <div className="button-content">
           {/* Conditionally render content based on isActive */}
-          {props.isActive ? ( 
+          {props.isActive ? (
             // Expanded content (Driver or Session menu)
-            <div className="submenu-content"> {/* Does not show up because its hidden in CSS, fix that */}
+            <div className="submenu-content">
+              {" "}
+              {/* Does not show up because its hidden in CSS, fix that */}
               {props.type === "Driver" && (
                 <div>
-                    <DriverMenu closeSubmenu={closeSubmenu}/>
+                  <DriverMenu closeSubmenu={closeSubmenu} />
                 </div>
               )}
               {props.type === "Session" && (
@@ -36,7 +37,6 @@ export default function MainButton(props) {
                 </div>
               )}
             </div>
-            
           ) : (
             // Collapsed content (just the button label)
             <h2>{props.type}</h2>
