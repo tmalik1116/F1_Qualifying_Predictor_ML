63fd67ad44879acfdf6d99c7247758a3fdb6e2a2
Talha Malik <146396156+tmalik1116@users.noreply.github.com>
2025-05-24 04:07:41 -0400

    Revert "changed style"
    This reverts commit f49f25a9e241477f911b04f6494d8b3c13afcd4e.

--- f1-app/src/SessionResultsModal.js
+++ f1-app/src/SessionResultsModal.js
@@ -3,9 +3,9 @@ import "./App.css";
 
 const SessionResultsModal = ({ isOpen, onClose, results }) => {
   return (
-    <dialog className="session-results-modal" style="background-colour: #2e2e2e" open={isOpen}>
-      <div className="modal-content" style="background-color: #2e2e2e">
-        <h2 className="modal-title" style="color: white">Session Results</h2>
+    <dialog className="session-results-modal" open={isOpen}>
+      <div className="modal-content">
+        <h2 className="modal-title">Session Results</h2>
         <div className="results-table">
           {results.map((result, index) => (
             <div key={index} className="result-row">
